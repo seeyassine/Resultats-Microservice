acac71bb6696df7ca546d0e962330b9f56be8f9c
nouhaila <nohaila.chkhonty@gmail.com>
2024-12-18 14:46:35 +0100

    hosting data base

--- db/db.js
+++ db/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Use environment variables for the MySQL connection configuration
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST || 'localhost', // Default to 'localhost' if not provided
+    host: process.env.MYSQL_HOST || '178.16.129.132', // Default to 'localhost' if not provided
     user: process.env.MYSQL_USER || 'root',      // Default to 'root'
-    password: process.env.MYSQL_PASSWORD || 'nano@password',  // Default to an empty password
+    password: process.env.MYSQL_PASSWORD || '',  // Default to an empty password
     database: process.env.MYSQL_DATABASE || 'my_db', // Default database name
-    port: process.env.MYSQL_PORT || 3306,        // Default port
+    port: process.env.MYSQL_PORT || 3307,        // Default port
     waitForConnections: true,                    // Enable connection pool
     //connectionLimit: 10                          // Adjust as necessary for your workload
 });
